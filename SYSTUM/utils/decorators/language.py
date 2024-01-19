c9cfbfe22e82fb0a7d966097eb510d4337b8645d
Officialhekarop <115996794+Officialhekarop@users.noreply.github.com>
2024-01-19 22:40:52 +0530

    Update language.py

--- SYSTUM/utils/decorators/language.py
+++ SYSTUM/utils/decorators/language.py
@@ -1,8 +1,6 @@
-from strings import get_string
-
-from SYSTUM import app
 from SYSTUM.misc import SUDOERS
 from SYSTUM.utils.database import get_lang, is_maintenance
+from SYSTUM import get_string
 
 
 def language(mystic):
