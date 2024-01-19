5cac6eda7626349267f25ef7b406ff767e3f719a
Officialhekarop <115996794+Officialhekarop@users.noreply.github.com>
2024-01-19 20:53:28 +0530

    Update queue.py

--- SYSTUM/plugins/tools/queue.py
+++ SYSTUM/plugins/tools/queue.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 import config
 from SYSTUM import app
 from SYSTUM.misc import db
-from SYSTUM.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from SYSTUM.utils import KINGBin, get_channeplayCB, seconds_to_min
 from SYSTUM.utils.database import get_cmode, is_active_chat, is_music_playing
 from SYSTUM.utils.decorators.language import language, languageCB
 from SYSTUM.utils.inline import queue_back_markup, queue_markup
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await AnonyBin(msg)
+        link = await KINGBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
