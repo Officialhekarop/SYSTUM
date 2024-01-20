7cdbf76ce1cfa96fdb11b2c7a5995da23da75637
Officialhekarop <115996794+Officialhekarop@users.noreply.github.com>
2024-01-20 15:25:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,6 +30,8 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6258877205))
 
+REPO_LINKE = getenv("REPO_LINKE", "https://github.com/WCGKING/SYSTUM")
+
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
